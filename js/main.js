29ae03c5826a113a73d043b59700b5e86683b5ff
Susan Pommer <kspommer@gmail.com>
2018-08-04 11:30:06 -0700

    Added key for mapbox

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1Ijoic3BvbSIsImEiOiJjamtmbzQ4Ym8wYWx0M3dsZW1yNTQ3cnM3In0.LhsYkhst4JdQgj8MZobDuw',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
